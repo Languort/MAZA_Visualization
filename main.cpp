c9dbe73e0ace9c86529eb9d4c65e3d7b660b2213
Languort <861799831@qq.com>
2023-12-14 01:57:45 +0800

    可视化迷宫第二版 对代码进行优化，提高运行效率 添加更详细注释 提高可读性

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-    #include"easyx/include/easyx.h"
+#include"easyx/include/easyx.h"
 #include <conio.h>
 #include<ctime>
 #include<vector>
@@ -7,64 +7,34 @@
 
 using namespace std;
 
-/*
- *
---------迷宫 自动生成与自动寻路可视化程序--------
 
- 目录
- 1. 初始化变量
-    1.1 初始化地图变量
-    1.2 定义地图元素
-    1.3 定义迷宫大小
-    1.4 定义颜色
-    1.5 定义迷宫格子
-    1.6 定义二维数组迷宫
-    1.7 定义动态数组
-    1.8 函数声明
-
- 2. 迷宫生成
-    2.1 Prim算法生成迷宫
- 3. 迷宫寻路
-    3.1 DFS算法寻路
-
- 4. 可视化
-    4.1 初始化
-    4.2 迷宫生成可视化
-    4.3 迷宫寻路可视化
-
-----------------------------------------------
-*/
-
-
-//1.0 初始化变量
-
-//1.1 画面初始化
+//标准量定义
 #define WIDTH 510  //画面宽度
 #define HEIGHT 510  //画面高度
 #define Block_xy 10 //方格边长
+#define ROAD 0
+#define WALL 1
+#define WAIT 2
+#define WAY 3
+#define PREWAY 4
+#define THEWAY 5
 
-//1.2 定义地图元素
-#define ROAD 0  // 路
-#define WALL 1  // 墙壁
-#define WAIT 2  // 墙壁临界块
-#define THEWAY 3    // 实际路径
-#define PREWAY 4    // 遍历路径
 
-//1.3 定义迷宫大小
+//迷宫大小定义
 const int Block_wid = WIDTH / Block_xy; // x轴方格个数
 const int Block_hei = HEIGHT / Block_xy; // y轴方格个数
 
 
-//1.4 定义颜色
+//颜色定义
 #define air COLORREF RGB(50,50,50) //空气颜色
-#define side COLORREF RGB(255,255,255)//墙壁颜色
-#define PreSelect COLORREF RGB(0,120,255)  //预选颜色
+#define side COLORREF RGB(225,225,225)//墙壁颜色
+#define PreSelect COLORREF RGB(0,50,255)  //预选颜色
 #define Select COLORREF RGB(255,0,0)   //选中颜色
-#define preway COLORREF RGB(0,255,0) //遍历路颜色
-#define way COLORREF RGB(0,100,0)// 实际寻路路径颜色
+#define preway COLORREF RGB(0,100,0) //预选路颜色
+#define theway COLORREF RGB(0,255,0)// 寻路路径颜色
 
 
-//1.5 定义迷宫格子
+//定义迷宫格子
 struct block{
 public:
     int x;
@@ -76,14 +46,14 @@ public:
 };
 
 
-//1.6 定义二维数组迷宫
+//定义二维数组迷宫
 int MAZA[Block_wid][Block_hei];
 
-//1.7 定义动态数组(vector)
+//未学 动态数组
 vector<block*> openlist;//待选列表
 vector<block*> list;
 
-//1.8 函数声明
+//函数声明
 void clearscreen(COLORREF RGB);//清空地图为RGB颜色(带有网格)
 void drawpixel(int x, int y, COLORREF RGB);//在x，y位置画颜色为RGB的方格
 void initializeMaza();  //初始化迷宫生成
@@ -91,62 +61,77 @@ void join(int x, int y);//将x,y四周是墙壁的路点加入待选列表
 void display();//刷新屏幕
 void prim_main();//prim主循环
 void through(int x, int y);//将x,y与它四周一个随机已经变成路的路点打通
-void displayPath();//绘制寻路路线
-void displaySearch();//
+void displaying();//绘制寻路路线
+void displayed();
+void displayway();
+
+/*********/
+
+/*
+ *
+--------迷宫 自动生成与自动寻路可视化程序--------
+
+ 目录
+ 1.0 初始化变量
+    1.1 初始化地图变量
+ 2.0 迷宫生成
+    2.1 Prim算法生成迷宫
+ 3.0 迷宫寻路
+    3.1 DFS算法寻路
+
+ 4.0 可视化
+    4.1 初始化
+    4.2 迷宫生成可视化
+    4.3 迷宫寻路可视化
+
+----------------------------------------------
+*/
 
 
-//2
+//2.1迷宫生成初始化
 
-//2.1 迷宫生成初始化
 void initializeMaza()
 {
     openlist.clear();
-    // 2.1.1 初始化地图全部设置为WALL
+    // 初始化地图全部设置为WALL
     for(int y = 0 ;y < Block_hei;y++){
         for (int x = 0;x < Block_wid;x++){
             MAZA[x][y] = WALL;
         }
     }
-    //2.1.2 将（0，0）点设置为路
+    /*随机选一个路点，将它变成路*/
     MAZA[0][0] = ROAD;
-
-    //2.1.3 将路点四周不是路的路点加入待选列表
+/*将路点四周不是路的路点加入待选列表*/
     join(0, 0);
-    //2.1.4 执行Prim算法
     prim_main();
 }
 
 
-// 2.2 Prim算法生成迷宫
+
 void prim_main() {
-    while (openlist.size() > 0) {   //while (!openlist.empty()) { 存疑
-        // 2.2.1 从openlist中随机生成一个点A
+    while (openlist.size() > 0) { //while (!openlist.empty()) { 存疑
+        //从openlist中选一个路点
         int ARandomPoint = rand() % openlist.size();
-        // 2.2.2 将生成的点A存入block中
         block *A = openlist[ARandomPoint];
-        //2.2.3 将随机点A与其周围一个随机的路点连接
+        //将随机点A与其周围一个随机的路点连接
         through(A->x, A->y);
-        //2.2.4 再将A点变成路点
+        //再将A点变成路点
         MAZA[A->x][A->y] = ROAD;
-        //2.2.5 再再将A点周围不是路的路点加入待选组 且不要加入重复路点
+        //再再将A点周围不是路的路点加入待选组 且不要加入重复路点
         join(A->x, A->y);
-        //2.2.6 即可从待选组中移除A点
+        //即可从待选组中移除A点
         openlist.erase(openlist.begin() + ARandomPoint);
-        //2.2.7 绘制地图选择方格
         drawpixel(A->x, A->y,Select);
         FlushBatchDraw(); //批量绘制 防止闪屏
-        //2.2.8 进行绘制
         display();
     }
-    openlist.clear(); //清理openlist
+    openlist.clear(); //？
     EndBatchDraw(); //关闭批量绘图
 }
 
-// 2.3 对随机pick点进行扩展逐渐生成迷宫
+
 void through(int x,int y){
     list.clear();
-    //2.3.1 对点格的相间隔点进行判断 如果两点可以连通则在中间形成ROAD
-    //上
     if (y - 2 >= 0 && MAZA[x][y - 2] == ROAD) {
         block* a = new block(x, y - 1);
         list.push_back(a);
@@ -167,17 +152,16 @@ void through(int x,int y){
         block* d = new block(x + 1, y);
         list.push_back(d);
     }
+    if (!list.empty()) {
+        int BRandomPoint = rand() % list.size();
+        block *B = list[BRandomPoint];
 
-    //2.3.2 生成随机点B
-    int BRandomPoint = rand() % list.size();
-    block* B = list[BRandomPoint];
-
-
-    //2.3.3 将X Y 与B点连通
-    MAZA[B->x][B->y] = ROAD;
+        // 将X Y 与B点连通
+        MAZA[B->x][B->y] = ROAD;
+    }
 }
 
-//2.4
+
 void join(int x , int y) {
     // 上面的格子
     if (y - 2 >= 0 && MAZA[x][y - 2] == WALL) {
@@ -205,7 +189,6 @@ void join(int x , int y) {
         MAZA[x + 2][y] = WAIT;
     }
 }
-
 
 // 3. DFS深度优先算法寻路
 
@@ -240,8 +223,8 @@ void push(int x,int y,Stack s){
         else p->next = s->next; //否则将p插入头节点之后
         s->next = p;
     }
-   // else{
-  //      cout<<"申请空间失败"<<endl;
+    // else{
+    //      cout<<"申请空间失败"<<endl;
 }
 
 //3.1.5 元素出栈函数
@@ -281,99 +264,38 @@ int flag=0;//用于记录是否寻路到终点
 void MazaPathDFS(int x,int y,int endx,int endy,int Block_wid,int Block_hei, Stack s){
     int newx,newy,i;
     //对四个方向进行寻路
-        for(i=0;i<4;i++) {
-            newx = x + direction[i][0];
-            newy = y + direction[i][1];
-            if (newx >= 0 && newx < Block_wid && newy >= 0 && newy < Block_hei && MAZA[newx][newy] == 0) {
-                //判断下一个点是否在地图外
-                push(newx, newy, s);
-                MAZA[newx][newy] = THEWAY;
-                displaySearch();
-                Sleep(10);
-                if (newx == endx && newy == endy) {
-                    //判断是否已经走到终点
-                    flag++; //标志
-                    displayPath();  //绘制路线
-                    MAZA[newx][newy] = 0;   //将终点设为WALL
-                    pop(s);     //出栈
+    for(i=0;i<4;i++) {
+        newx = x + direction[i][0];
+        newy = y + direction[i][1];
+        if (newx >= 0 && newx < Block_wid && newy >= 0 && newy < Block_hei && MAZA[newx][newy] == 0) {
+            //判断下一个点是否在地图外
+            push(newx, newy, s);
+            MAZA[newx][newy] = WAY;
+            displaying();
+            Sleep(5);
+            if (newx == endx && newy == endy) {
+                //判断是否已经走到终点
+                flag++; //标志
+                displayed();  //绘制路线
+                MAZA[newx][newy] = 0;   //将终点设为WALL
+                pop(s);     //出栈
 
-                }
-                else {
-                    MazaPathDFS(newx, newy, endx, endy, Block_wid, Block_hei, s); //自我嵌套实现递归
-                    if (flag > 0) {
-                        return;
-                    }
+            }
+            else {
+                MazaPathDFS(newx, newy, endx, endy, Block_wid, Block_hei, s); //自我嵌套实现递归
+                if (flag > 0) {
+                    return;
                 }
             }
-       }
+        }
+    }
     MAZA[x][y] = 0;
     pop(s);
 }
 
 
-//4.可视化
-
-//4.1 绘制像素格 基本定义
-void drawpixel(int x,int y,COLORREF RGB)
-{
-    setfillcolor(RGB);
-    setlinecolor(COLORREF RGB(0,0,0));
-    fillrectangle(x*Block_xy,y*Block_xy,x*Block_xy + Block_xy,y*Block_xy+Block_xy);
-
-}
-
-//4.2 绘制函数 对二维数组进行遍历 绘制迷宫地图
-void display(){
-    for(int y = 0 ; y < Block_hei ; y++){
-        for(int x = 0;x<Block_wid;x++)
-        {
-
-            if(MAZA[x][y] ==  WALL){    //绘制墙壁
-                drawpixel(x,y,side);
-            }
-            else if(MAZA[x][y] == ROAD) {   //绘制路径
-                drawpixel(x, y, air);
-            }
-            else if(MAZA[x][y] == WAIT){    //绘制路径预选格子
-                drawpixel(x,y,PreSelect);
-            }
-        }
-
-    }
-    FlushBatchDraw();
-}
-
-//4.3 绘制路径 对分析过的二维数组再次进行遍历 绘制遍历的最终路线
-void displayPath(){
-    for(int y = 0; y < Block_hei ; y++){
-        for(int x = 0;x<Block_wid;x++)
-        {
-            if(MAZA[x][y] == THEWAY) {  //绘制路线
-                drawpixel(x, y, preway);
-            }
-        }
-    }
-    FlushBatchDraw();
-}
-
-//4.4 绘制遍历过程 绘制遍历时经过的路线
-void displaySearch(){
-    for(int y = 0; y < Block_hei ; y++){
-        for(int x = 0;x<Block_wid;x++)
-        {
-            if(MAZA[x][y] == THEWAY) {
-                drawpixel(x, y, way);
-            }
-            else if(MAZA[x][y] == PREWAY){
-                drawpixel(x,y,preway);
-            }
-        }
-    }
-    FlushBatchDraw();
-}
-
 //5. DFS部分主函数
-void initializePath(){ //初始化绘制路径
+void initializePathDFS(){ //初始化绘制路径
 //5.1 初始化 起点 终点
     int xbegin  = 0;
     int ybegin  = 0;
@@ -387,23 +309,274 @@ void initializePath(){ //初始化绘制路径
 
 
 
+//BFS
 
-int main() {
+typedef struct {
+    int row;
+    int col;
+} Point;
 
-    //1.生成迷宫
+typedef struct {
+    Point point;
+    int distance;
+} QueueNode;
+
+typedef struct {
+    QueueNode* array;
+    int front;
+    int rear;
+} Queue;
+
+Queue* createQueue(int size) {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->array = (QueueNode*)malloc(size * sizeof(QueueNode));
+    queue->front = -1;
+    queue->rear = -1;
+    return queue;
+}
+
+void enqueue(Queue* queue, Point point, int distance) {
+    QueueNode newNode;
+    newNode.point = point;
+    newNode.distance = distance;
+
+    if (queue->front == -1) {
+        queue->front = 0;
+    }
+
+    queue->rear++;
+    queue->array[queue->rear] = newNode;
+}
+
+QueueNode dequeue(Queue* queue) {
+    QueueNode node = queue->array[queue->front];
+
+    if (queue->front == queue->rear) {
+        queue->front = -1;
+        queue->rear = -1;
+    } else {
+        queue->front++;
+    }
+
+    return node;
+}
+
+int isValid(int row, int col, int visited[Block_wid][Block_hei]) {
+    return (row >= 0 && row < Block_wid && col >= 0 && col < Block_hei && !visited[row][col]);
+}
+
+void printShortestPath(Point source, Point destination, Point parent[Block_wid][Block_hei]) {
+    Point path[Block_wid * Block_hei];
+    int pathLength = 0;
+
+    Point current = destination;
+    while (!(current.row == source.row && current.col == source.col)) {
+        path[pathLength++] = current;
+        current = parent[current.row][current.col];
+    }
+
+//  绘制最短路径
+    for (int i = pathLength - 1; i >= 0; i--) {
+        int n;
+        int m;
+        n=path[i].row;
+        m=path[i].col;
+        MAZA[m][n] = THEWAY;
+        displayway();
+    }
+
+}
+
+void BFS(int maze[Block_wid][Block_hei], Point source, Point destination) {
+    //建立遍历过数组
+    int visited[Block_wid][Block_hei];
+    //建立父节点便于回溯
+    Point parent[Block_wid][Block_hei];
+    //将遍历数组和父节点进行初始化 防止出现意外数据导致错误
+    for (int i = 0; i < Block_wid; i++) {
+        for (int j = 0; j < Block_hei; j++) {
+            visited[i][j] = 0;
+            parent[i][j] = (Point){-1, -1};
+        }
+    }
+
+    //建立队列
+    Queue* queue = createQueue(Block_wid * Block_hei);
+    //将起点加入队列
+    enqueue(queue, source, 0);
+    //将起点加入遍历数组
+    visited[source.row][source.col] = 1;
+
+    //建立横纵遍历数组
+    int rowOffsets[] = {-1, 0, 1, 0};
+    int colOffsets[] = {0, 1, 0, -1};
+
+    //当队列不为空时
+    while (queue->front != -1) {
+        //取出队列中的第一个元素
+        QueueNode current = dequeue(queue);
+        //将队列中的第一个元素出队
+        Point currentPoint = current.point;
+        //将队列中的第一个元素的距离出队
+        int currentDistance = current.distance;
+        //当队列中的第一个元素为终点时
+        if (currentPoint.row == destination.row && currentPoint.col == destination.col) {
+            //打印最短路径
+            printShortestPath(source, destination, parent);
+            break;
+        }
+        //当队列中的第一个元素不为终点时
+        for (int i = 0; i < 4; i++) {
+            int newRow = currentPoint.row + rowOffsets[i];
+            int newCol = currentPoint.col + colOffsets[i];
+
+            //当队列中的第一个元素的四周有路时
+            if (isValid(newRow, newCol, visited) && maze[newRow][newCol] == 0) {
+                //将四周的路加入队列
+                enqueue(queue, (Point){newRow, newCol}, currentDistance + 1);
+                //将四周的路加入遍历数组
+                visited[newRow][newCol] = 1;
+                //将四周的路加入父节点
+                parent[newRow][newCol] = currentPoint;
+                MAZA[newRow][newCol] = PREWAY;
+                displayway();
+                Sleep(5);
+            }
+        }
+    }
+
+    free(queue->array);
+    free(queue);
+}
+
+
+void drawpixel(int x,int y,COLORREF RGB)
+{
+    setfillcolor(RGB);
+    setlinecolor(COLORREF RGB(0,0,0));
+    fillrectangle(x*Block_xy,y*Block_xy,x*Block_xy + Block_xy,y*Block_xy+Block_xy);
+}
+
+
+void display(){ //绘制
+
+    //绘制迷宫
+    for(int y = 0 ; y < Block_hei ; y++){
+        for(int x = 0;x<Block_wid;x++)
+        {
+            if(MAZA[x][y] ==  WALL){
+                drawpixel(x,y,side);
+            }
+            else if(MAZA[x][y] == ROAD) {
+                drawpixel(x, y, air);
+            }
+            else if(MAZA[x][y] == WAIT){
+                drawpixel(x,y,PreSelect);
+            }
+
+        }
+
+    }
+    FlushBatchDraw();
+}
+
+void displaying(){
+    //绘制路径
+    for(int y = 0; y < Block_hei ; y++){
+        for(int x = 0;x<Block_wid;x++)
+        {
+            if(MAZA[x][y] == WAY) {  //绘制路线
+                drawpixel(x, y, preway);
+            }
+        }
+    }
+    FlushBatchDraw();
+}
+
+void displayed(){
+    //绘制路径
+    for(int y = 0; y < Block_hei ; y++){
+        for(int x = 0;x<Block_wid;x++)
+        {
+            if(MAZA[x][y] == WAY) {  //绘制路线
+                drawpixel(x, y, theway);
+            }
+        }
+    }
+    FlushBatchDraw();
+}
+
+void displayway(){
+    //绘制路径
+    for(int y = 0; y < Block_hei ; y++){
+        for(int x = 0;x<Block_wid;x++)
+        {
+            if(MAZA[x][y] == PREWAY) {
+                drawpixel(x, y, preway);
+            }
+            else if(MAZA[x][y] == THEWAY) {
+                drawpixel(x, y, theway);
+            }
+        }
+    }
+    FlushBatchDraw();
+}
+
+
+
+void initializePathBFS(){ //初始化绘制路径
+
+    int xbegin  = 0;
+    int ybegin  = 0;
+    int xend = 50;
+    int yend = 50;
+
+    Point start = {0, 0};
+    Point end = {50, 50};
+//    Queue q = createEmptyQueue();
+
+    BFS(MAZA,start,end);
+
+}
+
+void createRandomMAZA()
+{
     srand((unsigned)time(NULL));//随机数种子
     initgraph(WIDTH, HEIGHT);//创建绘图窗口
     BeginBatchDraw();//开始批量绘图
     cleardevice();//清空显示
-    initializeMaza(); //生成地图主函数
+    //绘制迷宫主函数
+    initializeMaza();
+}
 
 
-    //2.DFS迷宫寻路
-    BeginBatchDraw();//开始批量绘图
-    initializePath();//DFS寻找路径主函数
+int main() {
+
+
+    //随机生成第一个迷宫
+    createRandomMAZA();
+
+    //DFS寻找路径主函数
+    initializePathDFS();
     getch();
+    EndBatchDraw();
 
-    //3.BFS迷宫寻路
+    Sleep(500);
 
+    //随机生成第二个
+    createRandomMAZA();
+
+    //BFS寻找路径主函数
+    initializePathBFS();
+    getch();
+    EndBatchDraw();
+
+
+    closegraph();
+    return 0;
 
 }
+
+
+
+
